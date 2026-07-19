64e91850922bf5f5b8d7f529b1860e555aa2455e
Ashish <ashish.js05263@gmail.com>
2026-07-19 21:44:21 +0530

    U9

--- src/features/portfolio/components/profile-header.tsx
+++ src/features/portfolio/components/profile-header.tsx
@@ -11,37 +11,37 @@ import { VerifiedIcon } from "./verified-icon"
 export function ProfileHeader() {
   return (
     <div className="screen-line-bottom grid grid-cols-[auto_1fr] grid-rows-[1fr_auto] border-x border-line">
-      <figure className="relative col-span-2 p-2 sm:col-span-1 sm:col-start-2 sm:p-4">
-        <iframe
-          src="https://assets.pinterest.com/ext/embed.html?id=59180182597377985"
-          height="295"
-          width="345"
-          frameBorder="0"
-          scrolling="no"
+      <figure className="relative col-span-2 overflow-hidden sm:col-span-1 sm:col-start-2">
+        <video
+          src="/videmp4.mp4"
+          autoPlay
+          loop
+          muted
+          playsInline
+          className="h-auto w-full"
         />
-        <figcaption className="pointer-events-none absolute right-2 bottom-2 font-mono text-xs leading-none text-zinc-400 select-none sm:right-4 dark:text-zinc-700">
-          <ScrambleText text={USER.displayName} />
+        <figcaption className="pointer-events-none absolute bottom-2 left-2 sm:bottom-4 sm:left-4">
+          <ScrambleText
+            text={USER.displayName}
+            className="text-[2rem]/none font-medium tracking-tight"
+          />
         </figcaption>
       </figure>
 
       <div className="flex flex-col sm:row-span-2 sm:row-start-1">
         <div className="screen-line-top mt-auto shrink-0 border-r border-line">
           <AvatarLightsToggle className="group/avatar-lights-toggle mx-0.5 my-0.75 flex outline-none">
-            <div className="relative grid place-items-center" style={{ width: "12.5rem", height: "12.5rem" }}>
-              <SpinningCircularText
-                text="Trying to be better"
-                fontSize="1.2rem"
-                charSpacing={1.5}
-                spinClassName="duration-[20s]"
-                style={{ "--sc-container-size": "12.5rem" } as React.CSSProperties}
+            <SpinningCircularText
+              text="Trying to do better"
+              fontSize="1.2rem"
+              charSpacing={2.5}
+              spinClassName="duration-[20s]"
+            >
+              <AvatarLights
+                className="ring-border ring-offset-background group-focus-visible/avatar-lights-toggle:ring-1 group-focus-visible/avatar-lights-toggle:ring-offset-2"
+                variants={USER.avatarVariants}
               />
-              <div className="pointer-events-none absolute inset-0 flex items-center justify-center">
-                <AvatarLights
-                  className="ring-border ring-offset-background group-focus-visible/avatar-lights-toggle:ring-1 group-focus-visible/avatar-lights-toggle:ring-offset-2"
-                  variants={USER.avatarVariants}
-                />
-              </div>
-            </div>
+            </SpinningCircularText>
           </AvatarLightsToggle>
         </div>
       </div>
@@ -49,12 +49,6 @@ export function ProfileHeader() {
       <div className="flex flex-col">
         <div className="z-1 mt-auto border-t border-line">
           <div className="flex items-center gap-2 pl-4">
-            <ScrambleText
-              as="h1"
-              text={USER.displayName}
-              className="-translate-y-px text-[2rem]/none font-medium tracking-tight"
-            />
-
             <VerifiedIcon className="size-4.5 select-none" aria-hidden />
 
             {USER.namePronunciationUrl && (
